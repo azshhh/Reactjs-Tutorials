b30bf681e59df39111b91f78c203e96422e50fe0
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-07 19:55:27 +0530

    Creating setState()

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,33 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Redbull'
+    };
+  }
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button onClick={() => {
+            this.setState({name: 'Mercedes'})
+            console.log(this.state);
+          }}>
+          Change Name
+        </button>
+      </header>
+      </div >
+    );
+  }
 }
 
-export default App;
+export default App; 
