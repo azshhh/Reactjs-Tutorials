b4b239c30587ba2bd58d2881edbcc2ba0df511a8
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-07 23:40:57 +0530

    40. Monsters Rolodex - Input Search Box Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,52 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        }, () => {
-          console.log(this.state);
-        })
-      )
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
-      <div className="App" >
-        {
-          this.state.monsters.map((monster) => {
-            return <div key={monster.id}>
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          })
-        }
-      </div >
+          );
+        })}
+      </div>
     );
   }
 }
 
-export default App; 
+export default App;
