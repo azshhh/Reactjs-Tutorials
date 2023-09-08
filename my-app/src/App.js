09659d8737997401cb574d66e284aac342815208
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-08 20:13:43 +0530

    47. Component props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -48,16 +48,7 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-
-        {/* {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })} */}
-
-        <CardList />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- my-app/src/components/card-list/card-list-component.jsx
+++ my-app/src/components/card-list/card-list-component.jsx
@@ -2,9 +2,17 @@ import { Component } from "react";
 
 class CardList extends Component {
   render() {
+    console.log('render');
+    const { monsters } = this.props;
+
     return (
       <div>
-        <h1>CardList Component</h1>
+        {monsters.map((monster) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+            <h6>{monster.email}</h6>
+          </div>
+        ))}
       </div>
     );
   }
