e56c5ae026d7fef8b260dd600fde2cd272dd40b2
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-09 19:51:09 +0530

    understanding useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,26 @@
 // import { Component } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list-component";
 import SearchBox from "./components/search-box/search-box-component";
 
 const App = () => {
-  const [searchField, setSearchField] = useState('a');
+  const [searchField, setSearchField] = useState("a");
   const [monsters, setMonsters] = useState([]);
+  
   console.log('render');
 
-
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((respone) => respone.json())
-    .then((users) => setMonsters(users));
-
+  useEffect(
+    () => {
+      console.log("useEffect");
+      // This callback triggers only when any value in Dependency-Array changes
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((respone) => respone.json())
+        .then((users) => setMonsters(users));
+    },
+    // Dependency Array, which takes state-value or props-value
+    []
+  );
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
@@ -28,7 +35,7 @@ const App = () => {
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
       <SearchBox
-        className='monsters-search-box'
+        className="monsters-search-box"
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
       />
