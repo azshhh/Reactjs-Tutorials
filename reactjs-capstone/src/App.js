c4e4c05dce1a731adabb5c1ebc492a938160ec6e
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-11 19:29:43 +0530

    Outlet Route example

--- reactjs-capstone/src/App.js
+++ reactjs-capstone/src/App.js
@@ -1,10 +1,16 @@
 import Home from "./routes/Home";
 import { Routes, Route } from "react-router-dom";
 
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
