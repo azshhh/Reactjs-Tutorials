cc4a8371037458f23845a72e0ceb2d8cddc25714
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-11 19:40:10 +0530

    Nested Route

--- reactjs-capstone/src/App.js
+++ reactjs-capstone/src/App.js
@@ -1,14 +1,30 @@
 import Home from "./routes/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navigation Bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const Shop = () => {
-  return <h1>Shop page</h1>;
+  return (
+    <div>
+      <h1>Shop page</h1>
+    </div>
+  );
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
