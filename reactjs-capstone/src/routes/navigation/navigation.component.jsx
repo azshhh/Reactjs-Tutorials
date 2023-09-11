6d50c2794d2a5e0192474fcd5acc88eddba05526
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-11 20:55:37 +0530

    Adding Logo

--- reactjs-capstone/src/routes/navigation/navigation.component.jsx
+++ reactjs-capstone/src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo" to="/">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
