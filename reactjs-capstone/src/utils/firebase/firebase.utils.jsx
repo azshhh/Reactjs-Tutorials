845008600e066878c72f00b06fe35adb2869305c
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-12 19:51:03 +0530

    Firebase signInWithGooglePopup

---
+++ reactjs-capstone/src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,35 @@
+// we importinf firebase/app to initialize our local app
+import { initializeApp } from "firebase/app";
+
+// to handle authentication we are importing several auths from firebase
+import {
+  getAuth,
+  //   signInWithRedirect,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB07MylcuutHrjDCDPHJ2MjUZEL8N7ampg",
+  authDomain: "capstone-db-11c03.firebaseapp.com",
+  projectId: "capstone-db-11c03",
+  storageBucket: "capstone-db-11c03.appspot.com",
+  messagingSenderId: "891145211329",
+  appId: "1:891145211329:web:42cf7281e433e1b0d9f340",
+};
+
+// initializing our app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// provider is an instance of GoogleAuthProvider, there can be multiple instances
+const provider = new GoogleAuthProvider();
+
+// now we are setting up a parameter by which we authorize a user
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+// default auth provided by firebase to authorize user with provided parameter
+export const auth = getAuth(firebaseApp);
+// this generates a Google Popup to signin
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
