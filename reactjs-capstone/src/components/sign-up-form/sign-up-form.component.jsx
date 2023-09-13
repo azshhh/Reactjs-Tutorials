79e25258db5be584f19b50a4396269ccd5865d4b
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-13 15:26:04 +0530

    handleChange complete

--- reactjs-capstone/src/components/sign-up-form/sign-up-form.component.jsx
+++ reactjs-capstone/src/components/sign-up-form/sign-up-form.component.jsx
@@ -10,21 +10,50 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { name, email, password, confirmPassword } = formFields;
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div>
       <h1>Sign-up with your email and password</h1>
       <form onSubmit={() => {}}>
         <label>Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="name"
+          value={name}
+        />
 
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="changePassword"
+          value={confirmPassword}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
