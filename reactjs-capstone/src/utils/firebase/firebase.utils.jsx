51d37a07e31dc9a883a89843af3d133fd62130a7
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-13 12:03:45 +0530

    Creating user in db

--- reactjs-capstone/src/utils/firebase/firebase.utils.jsx
+++ reactjs-capstone/src/utils/firebase/firebase.utils.jsx
@@ -1,9 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,25 +11,33 @@ const firebaseConfig = {
   appId: "1:891145211329:web:42cf7281e433e1b0d9f340",
 };
 
+// Initialize app with provided firebaseConfig
 const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
+// Providing GoogleAuth method with 'select_account' variable
 provider.setCustomParameters({
   prompt: "select_account",
 });
 
+// Google provides a Popup to sign with google account
 export const auth = getAuth(firebaseApp);
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
+// Once authorization completes, we get access to the firebase-db
 export const db = getFirestore();
+
+// Creating userDocumentFromAuthorization created above
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
   console.log(userDocRef);
 
+  // To check the current existance of the user, we create 'userSnapshot', which getDoc for instace user
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
   console.log(userSnapshot.exists());
 
+  // if not exits, then create one
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -48,5 +52,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
   }
 
+  // exists? then return
   return userDocRef;
 };
