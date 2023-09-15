286f3df788b894e29ea6ee215e12b496dd88ebc1
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-16 00:16:58 +0530

    UserContext stage-2

--- reactjs-capstone/src/components/context/user.context.jsx
+++ reactjs-capstone/src/components/context/user.context.jsx
@@ -1,10 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 // This is actual values, or Context where user values are stored
 export const UserContext = createContext({});
 
 // This provider, provides the user values to the child it recieves (say <App />)
 export const UserProvider = ({ children }) => {
-    // Here value={}, which means this UserContext.Provider holds the values from UserContext to provide it to children
-  return <UserContext.Provider value={}>{children}</UserContext.Provider>;
+  //We want the currentUser value, initially it is null, using useState hook we gonna update the user
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // UserContext.Provider will hold user values to provide to its children
+  const value = { currentUser, setCurrentUser };
+
+  // Here value={}, which means this UserContext.Provider holds the values from UserContext to provide it to children
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
